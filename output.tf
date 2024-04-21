9b611fc7afbb5acab10e1f779fbfe075fa1fedb6
amit <amit@ow.com>
2024-04-21 17:42:54 +0000

    adD

--- output.tf
+++ output.tf
@@ -7,4 +7,9 @@ value = aws_subnet.subnet-1.id
 output "aws_internet_gateway" {
 value = aws_internet_gateway.igw.id
 }
-
+output "my_key_pair_name" {
+  value = aws_key_pair.vm_ssh_key.key_name
+}
+output "security_group_id_array" {
+value = aws_security_group.open_ports.*.id
+}
